8d2b40ff4a811044a8af943c5e28bdf85d902aa5
protpr0t <75566875+protpr0t@users.noreply.github.com>
2026-09-21 23:55:41 +0700

    Update repository URLs and cleanup commands

--- crdroid_a15.sh
+++ crdroid_a15.sh
@@ -9,11 +9,11 @@ rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/obj/ETC/Changelog.txt_intermediates \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
-# Clone crdroid 15.0
+# Clone crdroid
 
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --depth=1 --git-lfs --no-clone-bundle
 
-git clone https://github.com/protpr0t/local_manifest_alphaplus.git --depth 1 -b alphaplus-crdroid15 .repo/local_manifests
+git clone https://github.com/Xtrakari/local_manifest_alphaplus.git --depth 1 -b alphaplus-crdroid15 .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/Xtrakari/local_manifest_alphaplus.git
  fi

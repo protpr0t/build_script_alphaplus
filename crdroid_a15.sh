2bc66c789be5ef6b40dfd2faf690442b0247a2ae
protpr0t <75566875+protpr0t@users.noreply.github.com>
2026-09-22 00:17:13 +0700

    Refactor crdroid_a15.sh for crDroid 15.0 setup
    Updated the script to initialize crDroid 15.0 and download local manifest directly using curl.

--- crdroid_a15.sh
+++ crdroid_a15.sh
@@ -4,20 +4,20 @@ rm -rf kernel/lge
 rm -rf vendor/lge
 rm -rf hardware/lge
 rm -rf out/target/product/alphaplus
-# Cleanup previous changelog to make it always fresh
+
+# Cleanup previous changelog
 rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/obj/ETC/Changelog.txt_intermediates \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
-# Clone crdroid
-
+# Initialize crDroid 15.0
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --depth=1 --git-lfs --no-clone-bundle
 
-git clone https://github.com/Xtrakari/local_manifest_alphaplus.git --depth 1 -b alphaplus-crdroid15 .repo/local_manifests
-if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/Xtrakari/local_manifest_alphaplus.git
- fi
+# Download local manifest directly using curl
+mkdir -p .repo/local_manifests
+curl -sL https://raw.githubusercontent.com/protpr0t/local_manifest_alphaplus/alphaplus-crdroid15/local_manifest.xml -o .repo/local_manifests/alphaplus.xml
 
+# Crave resync and start build
 /opt/crave/resync.sh
 
 source build/envsetup.sh

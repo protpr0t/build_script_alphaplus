7c4f40bb1297fd0ac98477fc2e69c4ee25d92e53
protpr0t <75566875+protpr0t@users.noreply.github.com>
2026-09-22 22:15:53 +0700

    Update crDroid build script for version 15.0

--- crdroid_a15.sh
+++ crdroid_a15.sh
@@ -5,21 +5,18 @@ rm -rf vendor/lge
 rm -rf hardware/lge
 rm -rf out/target/product/alphaplus
 
-# Cleanup previous changelog
 rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/obj/ETC/Changelog.txt_intermediates \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
-# Initialize crDroid 15.0
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --depth=1 --git-lfs --no-clone-bundle
 
-# Download local manifest directly using curl
 mkdir -p .repo/local_manifests
-curl -sL https://raw.githubusercontent.com/protpr0t/local_manifest_alphaplus/alphaplus-crdroid15/local_manifest.xml -o .repo/local_manifests/alphaplus.xml
+curl -s -L "https://raw.githubusercontent.com/protpr0t/local_manifest_alphaplus/main/local_manifest.xml" -o .repo/local_manifests/alphaplus.xml
 
-# Crave resync and start build
 /opt/crave/resync.sh
 
 source build/envsetup.sh
 
-brunch alphaplus
+breakfast crdroid_alphaplus
+m bacon
